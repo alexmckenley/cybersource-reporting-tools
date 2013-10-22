bda2770e2f346074d3729a3439c63b75c451a79d
Alex's Work Computer <alexmckenley@gmail.com>
2013-10-22 15:54:56 -0700

    Changed how the request method works

--- queryAPI.js
+++ queryAPI.js
@@ -1,15 +1,19 @@
 var request = require('request');
 
-request.post('https://ebctest.cybersource.com/ebctest/TransactionExceptionDetailReportRequest.do', function(error, response, body){
-	console.log("Finished: ", body);
-})
-.form({
-	username:'test',
-	merchantID: 'mckenley1',
-	password: 'password123',
-	startDate: '2013-10-22',
-	endDate: '2013-10-23',
-	startTime: '10:45:00',
-	endTime: '06:00:00',
-	format: 'xml'
-});
+request.post(
+	{uri: 'https://ebctest.cybersource.com/ebctest/TransactionExceptionDetailReportRequest.do',
+	form: {	
+		username:'test',
+		merchantID: 'mckenley1',
+		password: 'password123',
+		startDate: '2013-10-22',
+		endDate: '2013-10-23',
+		startTime: '07:00:00',
+		endTime: '07:00:00',
+		format: 'xml'
+		}
+	},
+	function(error, response, body){
+		console.log("Finished: ", body);
+	}
+);
