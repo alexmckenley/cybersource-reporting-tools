a142e89736f9411d32cfd86cab6abdeb8a6159fb
Alex's Work Computer <alexmckenley@gmail.com>
2013-10-22 14:08:08 -0700

    Add Single Transaction query

--- queryAPI.js
+++ queryAPI.js
@@ -1,14 +1,15 @@
 var request = require('request');
 
-request.post('https://ebctest.cybersource.com/ebctest/TransactionExceptionDetailReportRequest.do')
+request.post('https://ebctest.cybersource.com/ebctest/TransactionExceptionDetailReportRequest.do', function(error, response, body){
+	console.log("Finished: ", body);
+})
 .form({
 	username:'test',
 	merchantID: 'mckenley1',
 	password: 'password123',
-	startDate: '2013-10-21',
-	endDate: '2013-10-22',
-	startTime: '07:00:00',
-	endTime: '07:00:00',
-	format: 'csv'
-})
-.pipe(process.stdout);
+	startDate: '2013-10-22',
+	endDate: '2013-10-23',
+	startTime: '10:45:00',
+	endTime: '06:00:00',
+	format: 'xml'
+});
